ed2c251871ec71ead36d9dd1c93d4287df6a12a6
ankitch <ankitchhetri0@gmail.com>
2018-02-07 16:52:46 +0545

    List model

--- apps/tanks/models.py
+++ apps/tanks/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 
+class List(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 # Create your models here.
 class Customer(models.Model):
     full_name = models.CharField(max_length=255, blank=True, null=True)
-    email = models.EmailField(max_length=255,blank=True, null=True)
-    phone = models.CharField(max_length=255,    blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
