cd4c636ee2019c774a5719efbb6d8ba045723b96
Roshan Shrestha <roshanshrestha01@gmail.com>
2018-05-14 12:38:42 +0545

    api.sendtank.com

--- mccc/settings/prod.py
+++ mccc/settings/prod.py
@@ -4,6 +4,6 @@ SECRET_KEY = 'nxg=f!y6$z6cti-j5mgjbc#92pb6%(e--9+b)1vvb_-(wyh-dd'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.sendtank.com', 'sendtank.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'api.sendtank.com']
 
 STATICFILES_DIRS = ()
