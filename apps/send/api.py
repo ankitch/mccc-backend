6f19ad48b71bcf2f13146c2ce36d3f420a30a638
ankitch <ankitchhetri0@gmail.com>
2018-05-15 10:13:53 +0545

    completed

--- apps/send/api.py
+++ apps/send/api.py
@@ -18,7 +18,10 @@ def email_to_ses(query, lists, campaign):
     for items in search_results:
         print(items.pk)
         print(items.email)
-        send = camp_email.format(url='http://127.0.0.1:8000/s1/' + camp_short + '/' + items.pk + "/" + str(campaign))
+        print('http://192.168.43.217:8000/s1/' + camp_short +
+              '/' + items.pk + "/" + str(campaign))
+        send = camp_email.format(url='http://192.168.43.217:8000/s1/' + camp_short +
+                                     '/' + items.pk + "/" + str(campaign))
         ses_mail = send_mail(camp_subj,
                              send,
                              'from@example.com',
@@ -56,7 +59,8 @@ def send_push_notification(query, lists, campaign):
 
     push_service = FCMNotification(
         api_key="AAAApd54CKA:APA91bHh60kTOjmJQP8qv8IcQtnkB3-uq-NZtmFGefneT3xlS5dfDEiPUVgjrOKVQdyamgTHn7vIfRvNI6I3vo6nwqW3KntsapmcJIFfJYfrs9a5bYT9VwaRMAVTb6Xzk0Wbm2L5ZEHT")
-    result = push_service.notify_multiple_devices(fcm_ids,  body_push, title_push,)
+    result = push_service.notify_multiple_devices(
+        fcm_ids, body_push, title_push, )
     print(result)
     print(query, lists)
     print(campaign)
@@ -82,7 +86,7 @@ def send_sms_fcm(campaign, segment):
         api_key=settings.FCM_API_KEY_SEND)
 
     result = push_service.single_device_data_message(
-        registration_id="cGM4Rrw6cVQ:APA91bHj-hbR2ZAeoQQqWcX2Ej8Bk2FhnKWsgCPs4-UJ8CSmiQHgPMzHYofhA5X3eqtXefD4JUGEkw_v9UZvxt3UZM7pOgW1bIE37vV-OvbbvJ8kAcsfOjjNc39nTdTItjYR2YZ7hALm",
+        registration_id="fM3638Gquro:APA91bGkS7RHyhsLh0hqKhUs-ju-mkGhT7wy63ajNCalwHKx52LIv0JP2TZY3Bcysly-nadlOjvCWHTWr6U6tFyKib_eojgXqHXkWMpO5MJG_IngGy_njyI1eaauYo8T2HKEOKH-8iXa",
         data_message=data_message)
     print(result)
     print(campaign, segment)
@@ -101,10 +105,12 @@ def send_sms(request, *args, **kwargs):
 def perform_search(query, lists):
     search_query = None
     if 'sex' in query:
-        search_query = SearchQuerySet().filter(SQ(lists=lists) & SQ(sex=query['sex']))
+        search_query = SearchQuerySet().filter(
+            SQ(lists=lists) & SQ(sex=query['sex']))
 
     elif 'age' in query:
-        search_query = SearchQuerySet().filter(SQ(lists=lists) & SQ(age=query['age']))
+        search_query = SearchQuerySet().filter(
+            SQ(lists=lists) & SQ(age=query['age']))
 
     return search_query
 
@@ -129,17 +135,21 @@ def schedule_campaign(request, *args, **kwargs):
     push_func = 'apps.send.api.send_push_notification'
 
     if channel == "SMS":
-        sch = schedule_sms(sms_func, campaigns, segments, next_run, sch_type, repeats, minutes)
+        sch = schedule_sms(sms_func, campaigns, segments, next_run, sch_type,
+                           repeats, minutes)
     elif channel == "Email":
-        sch = schedule_email_push(email_func, query, lists, campaigns, next_run, sch_type, repeats, minutes)
+        sch = schedule_email_push(
+            email_func, query, lists, campaigns, next_run, sch_type, repeats,
+            minutes)
 
     elif channel == "Push":
-        sch = schedule_email_push(push_func, query, lists, campaigns, next_run, sch_type, repeats, minutes)
+        sch = schedule_email_push(
+            push_func, query, lists, campaigns, next_run, sch_type, repeats,
+            minutes)
 
-    # Todo
-    # trigger all
     elif channel == "All":
-        sch = trigger_all(push_func, email_func, sms_func, campaigns, segments, query, lists, next_run, sch_type,
+        sch = trigger_all(push_func, email_func, sms_func, campaigns, segments,
+                          query, lists, next_run, sch_type,
                           repeats, minutes)
 
     return Response({'schedule': 'created'})
