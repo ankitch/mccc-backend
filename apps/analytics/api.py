2c8df20c7cacf0f793415991503464057aea9451
ankitch <ankitchhetri0@gmail.com>
2018-05-23 14:56:56 +0545

    updated

--- apps/analytics/api.py
+++ apps/analytics/api.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 
 from apps.analytics.models import ClickEvent
 from apps.tanks.models import Customer, Campaign, List
-from apps.url_shortner.models import ShortenedUrl   
+from apps.url_shortner.models import ShortenedUrl
 
 
 class DashboardAnalytics(APIView):
